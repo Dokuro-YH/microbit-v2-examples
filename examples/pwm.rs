1c54c0f41907a6192b5244dc9a21612bbcdbc038
yanhai <shimmly@live.cn>
2022-11-19 23:25:49 +0800

    feat: play music

--- examples/pwm.rs
+++ examples/pwm.rs
@@ -2,16 +2,31 @@
 #![no_std]
 
 use panic_halt as _;
-use rtt_target::rprintln;
 
-use cortex_m_rt::entry;
 use microbit::hal::gpio::Level;
 use microbit::hal::prelude::*;
-use microbit::hal::pwm::{Channel, CounterMode, Prescaler, Pwm, Seq};
+use microbit::hal::pwm::{Channel, CounterMode, Pwm, Seq};
 use microbit::hal::timer::Timer;
 use microbit::Board;
 
-#[entry]
+const TONES: [[u32; 14]; 3] = [
+    //   C    D     E     F     G     A     B
+    //   1.   2.    3.    4.    5.    6.    7.
+    [360000, 131, 147,  165,  175,  196,  221,  248,  278,  312,  330,  371,  416,  476],
+    //   1    2     3     4     5     6     7 
+    [360000, 262, 294,  330,  350,  393,  441,  496,  556,  624,  661,  742,  833,  935],
+    //   1'   2'    3'    4'    5'    6'    7'
+    [360000, 525, 589,  661,  700,  786,  882,  990,  1112, 1248, 1322, 1484, 1665, 1869],
+  ];
+
+pub const XIAO_XING_XING: [usize; 32] = [
+    1, 1, 5, 5, 6, 6, 5, 0, 
+    4, 4, 3, 3, 2, 2, 1, 0,
+    5, 5, 4, 4, 3, 3, 2, 0,
+    5, 5, 4, 4, 3, 3, 2, 0,
+];
+
+#[cortex_m_rt::entry]
 fn main() -> ! {
     rtt_target::rtt_init_print!();
 
@@ -27,62 +42,24 @@ fn main() -> ! {
         .set_counter_mode(CounterMode::UpAndDown)
         .set_seq_refresh(Seq::Seq0, 0)
         .set_seq_end_delay(Seq::Seq0, 0)
-        .set_prescaler(Prescaler::Div16)
         .set_period(1.hz())
-        .set_max_duty(u16::MAX)
+        .set_max_duty(100)
         .enable();
 
-    // duty = 音量
-    // freq = 音调
-
     let mut timer = Timer::new(board.TIMER0);
-
-    let mut max_duty = pwm.max_duty();
-    let mut duty = pwm.max_duty() / 7;
-    pwm.set_duty_on_common(duty);
+    let volume = 50;
 
     loop {
-        for i in music::music_1 {
-            rprintln!("max_duty {} duty {} {}hz ", max_duty, duty, i);
-            pwm.set_period(i.hz());
-            timer.delay_ms(3000_u32);
+        for note in XIAO_XING_XING {
+            let pitch = 1;
+            let tone = TONES[pitch][note];
+            let delay = 500_u32;
 
-            max_duty = pwm.max_duty();
-            duty = max_duty / 2;
-            pwm.set_duty_on_common(duty);
-            timer.delay_ms(100_u32);
+            pwm.set_period(tone.hz());
+            pwm.set_duty_on_common((pwm.max_duty() as u32 * volume as u32 / 100) as u16);
+
+            timer.delay_ms(delay);
         }
-
-        // pwm.set_duty_on_common(pwm.max_duty());
-        rprintln!("Restart!");
-        timer.delay_ms(3000_u32);
+        timer.delay_ms(1000_u32);
     }
-}
-
-mod music {
-    pub const D_DO: u32 = 262;
-    pub const D_RE: u32 = 294;
-    pub const D_MI: u32 = 330;
-    pub const D_FA: u32 = 349;
-    pub const D_SO: u32 = 392;
-    pub const D_LA: u32 = 440;
-    pub const D_SI: u32 = 494;
-
-    pub const M_DO: u32 = 523;
-    pub const M_RE: u32 = 587;
-    pub const M_MI: u32 = 659;
-    pub const M_FA: u32 = 698;
-    pub const M_SO: u32 = 784;
-    pub const M_LA: u32 = 880;
-    pub const M_SI: u32 = 988;
-
-    pub const H_DO: u32 = 1046;
-    pub const H_RE: u32 = 1157;
-    pub const H_MI: u32 = 1318;
-    pub const H_FA: u32 = 1397;
-    pub const H_SO: u32 = 1568;
-    pub const H_LA: u32 = 1760;
-    pub const H_SI: u32 = 1976;
-
-    pub const music_1: [u32; 7] = [M_DO, M_RE, M_MI, M_FA, M_SO, M_LA, M_SI];
 }
